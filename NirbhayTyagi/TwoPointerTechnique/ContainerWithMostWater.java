ded6cad90773887bafebc055702a09f2439caebc
Nirbhay-Tyagi <146717198+Nirbhay-Tyagi@users.noreply.github.com>
2025-02-26 23:03:30 +0530

    adding ContainerWithMostWater file

---
+++ NirbhayTyagi/TwoPointerTechnique/ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class ContainerWithMostWater{
+    public static int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int ans = 0;
+        while (i < j) {
+            int t = Math.min(height[i], height[j]) * (j - i);
+            ans = Math.max(ans, t);
+            if (height[i] < height[j]) {
+                ++i;
+            } else {
+                --j;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements of array");
+        int n = sc.nextInt();
+        int[] height = new int[n];
+        System.out.println("Enter the elements of array");
+        for(int i = 0; i < n; i++){
+            height[i] = sc.nextInt();
+        }
+        System.out.println("The maximum amount of water a container can store is:" + maxArea(height));
+
+    }
+}
